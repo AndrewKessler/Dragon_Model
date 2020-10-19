5b08b0f819e6e695a5574637ad66f1a16be5a826
AndrewKessler <andrewksslr@gmail.com>
2020-10-19 13:24:15 +0200

    adding gitignore

--- target/debug/deps/cfg_if-37f8ba33dea32053.d
+++
@@ -1,7 +0,0 @@
-C:\Rust_course\ultimate_rust_crash_course\exercise\code\dragon_pool\target\debug\deps\cfg_if-37f8ba33dea32053.rmeta: C:\Users\Andrew\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-0.1.10\src\lib.rs
-
-C:\Rust_course\ultimate_rust_crash_course\exercise\code\dragon_pool\target\debug\deps\libcfg_if-37f8ba33dea32053.rlib: C:\Users\Andrew\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-0.1.10\src\lib.rs
-
-C:\Rust_course\ultimate_rust_crash_course\exercise\code\dragon_pool\target\debug\deps\cfg_if-37f8ba33dea32053.d: C:\Users\Andrew\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-0.1.10\src\lib.rs
-
-C:\Users\Andrew\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-0.1.10\src\lib.rs:
--- target/debug/dragon_pool.d
+++
@@ -1,1 +0,0 @@
-C:\Rust_course\ultimate_rust_crash_course\exercise\code\dragon_pool\target\debug\dragon_pool.exe: C:\Rust_course\ultimate_rust_crash_course\exercise\code\dragon_pool\src\main.rs
